713024612121f1bdae9fbd05fffa1e9fa68734a5
kimgebin <vickylawrenz@gmail.com>
2025-06-13 11:35:23 +0700

    Add files via upload

--- data.js
+++ data.js
@@ -1,13 +1,8 @@
-
 const dataMimpi = {
   "ular": { tafsir: "Akan bertemu musuh atau rezeki", angka: ["06", "60", "906"] },
   "gigi copot": { tafsir: "Akan kehilangan orang dekat", angka: ["32", "132", "7132"] },
   "jatuh": { tafsir: "Tanda kegagalan sementara", angka: ["08", "108", "2108"] },
   "bayi": { tafsir: "Simbol awal baru atau tanggung jawab", angka: ["25", "125", "5125"] },
   "hantu": { tafsir: "Takut terhadap masa lalu", angka: ["13", "113", "3113"] },
-  "mantan": { tafsir: "Kenangan lama yang belum selesai", angka: ["27", "127", "3127"] },
-  "dikejar": { tafsir: "Menghindari masalah", angka: ["14", "114", "2114"] },
-  "menikah": { tafsir: "Komitmen atau perubahan hidup besar", angka: ["45", "145", "5145"] },
-  "terbang": { tafsir: "Ingin bebas atau ambisi tinggi", angka: ["09", "109", "4109"] },
-  "tenggelam": { tafsir: "Tenggelam dalam emosi atau tekanan", angka: ["19", "219", "4219"] }
+  "mantan": { tafsir: "Kenangan lama yang belum selesai", angka: ["27", "127", "3127"] }
 };
